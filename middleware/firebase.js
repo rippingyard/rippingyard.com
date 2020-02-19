18bb3db1e6be053af555876ee72ed43e1196578d
mcatm <tkcs@pelepop.com>
2020-02-19 19:35:36 +0900

    fix firebase. #1

--- middleware/firebase.js
+++ middleware/firebase.js
@@ -1,25 +1,27 @@
 import firebase from 'firebase/app'
 
+const config = process.env.FIREBASE_CONFIG
+
 require('firebase/firestore')
 require('firebase/auth')
 require('firebase/storage')
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: process.env.FIREBASE_APIKEY,
-    authDomain: process.env.FIREBASE_AUTHDOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASEURL,
-    projectId: process.env.FIREBASE_PROJECTID,
-    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGESENDERID,
-    appId: process.env.FIREBASE_APPID,
-    measurementId: process.env.FIREBASE_MEASUREMENTID,
+    apiKey:             config.apiKey,
+    authDomain:         config.authDomain,
+    databaseURL:        config.databaseURL,
+    projectId:          config.projectId,
+    storageBucket:      config.storageBucket,
+    messagingSenderId:  config.messagingSenderId,
+    appId:              config.appId,
+    measurementId:      config.measurementId,
   })
 }
 
 export default firebase
 
-// export const db = firebase.firestore()
-// export const auth = firebase.auth()
-// export const storage = firebase.storage()
-// export const timestamp = firebase.firestore.Timestamp
+export const db = firebase.firestore()
+export const auth = firebase.auth()
+export const storage = firebase.storage()
+export const timestamp = firebase.firestore.Timestamp
