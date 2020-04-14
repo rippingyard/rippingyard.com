33e1b955cd08929a2e82c0aac9fcb4e0314334b3
mcatm <tkcs@pelepop.com>
2020-04-15 00:35:01 +0900

    arrange DOM.

--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -8,16 +8,20 @@
       </b-navbar-item>
     </template>
     <template slot="end">
-      <b-navbar-item v-if="!$isAuthenticated($store)" :to="{ path: 'signin' }" tag="router-link">
-        ログイン
-      </b-navbar-item>
-      <b-navbar-item v-if="!$isAuthenticated($store)" :to="{ path: 'signup' }" tag="router-link">
-        新規登録
-      </b-navbar-item>
-      <b-navbar-dropdown v-if="$isAuthenticated($store)" label="ユーザー">
-        <b-navbar-item :to="{ path: 'home' }" tag="router-link">HOME</b-navbar-item>
-        <b-navbar-item @click="$signOut($store)">ログアウト</b-navbar-item>
-      </b-navbar-dropdown>
+      <template v-if="!$isAuthenticated($store)">
+        <b-navbar-item :to="{ path: 'signin' }" tag="router-link">
+          ログイン
+        </b-navbar-item>
+        <b-navbar-item :to="{ path: 'signup' }" tag="router-link">
+          新規登録
+        </b-navbar-item>
+      </template>
+      <template v-else>
+        <b-navbar-dropdown label="ユーザー">
+          <b-navbar-item :to="{ path: 'home' }" tag="router-link">HOME</b-navbar-item>
+          <b-navbar-item @click="$signOut($store)">ログアウト</b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
     </template>
   </b-navbar>
 </template>
