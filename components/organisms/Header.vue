2e4cb90cb0e2d9929cceb30a3548a0e09656c180
mcatm <tkcs@pelepop.com>
2020-04-15 02:24:47 +0900

    update header.

--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -9,18 +9,12 @@
     </template>
     <template slot="end">
       <template v-if="!$isAuthenticated($store)">
-        <b-navbar-item :to="{ path: 'signin' }" tag="router-link">
-          ログイン
-        </b-navbar-item>
-        <b-navbar-item :to="{ path: 'signup' }" tag="router-link">
-          新規登録
-        </b-navbar-item>
+        <b-navbar-item :to="{ path: 'signin' }" tag="router-link">ログイン</b-navbar-item>
+        <b-navbar-item :to="{ path: 'signup' }" tag="router-link">新規登録</b-navbar-item>
       </template>
       <template v-else>
-        <b-navbar-dropdown label="ユーザー">
-          <b-navbar-item :to="{ path: 'home' }" tag="router-link">ホーム</b-navbar-item>
-          <b-navbar-item @click="$signOut($store)">ログアウト</b-navbar-item>
-        </b-navbar-dropdown>
+        <b-navbar-item :to="{ path: 'home' }" tag="router-link">ホーム</b-navbar-item>
+        <b-navbar-item @click="$signOut($store)">ログアウト</b-navbar-item>
       </template>
     </template>
   </b-navbar>
