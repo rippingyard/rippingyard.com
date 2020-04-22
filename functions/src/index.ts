46be31e0499ca6304ff6663036a1cfc180409726
mcatm <tkcs@pelepop.com>
2020-04-22 21:46:33 +0900

    try ssr. #28

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,28 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+
+const { Nuxt } = require('nuxt')
+const app = require('express')()
+
+const nuxt = new Nuxt({
+  dev: false,
+  buildDir: '.nuxt',
+  build: {
+    publicPath: '/'
+  }
+})
+
+function handler(req, res) {
+  nuxt.renderRoute('/', { req }).then(result => {
+    res.send(result.html)
+  }).catch(e => {
+    res.send(e)
+  })
+}
+
+app.use(handler)
+exports.app = functions.https.onRequest(app)
+
 admin.initializeApp(functions.config().firebase);
 const firestore = admin.firestore();
 
