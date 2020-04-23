43baf129f8cb8653b2540eb781032600df99fa57
mcatm <tkcs@pelepop.com>
2020-04-23 19:53:36 +0900

    wip. #28

--- functions/src/ssr.ts
+++ functions/src/ssr.ts
@@ -3,11 +3,11 @@ const { Nuxt } = require('nuxt');
 
 const app = express();
 const nuxt = new Nuxt({
-    dev: false,
-    buildDir: '.nuxt',
-    build: {
-      publicPath: '../dist'
-    }
+  dev: false,
+  buildDir: '.nuxt',
+  build: {
+    publicPath: '/dist/'
+  }
 });
 
 app.use(nuxt.render);
