c8f40577f8f2150a175de7dc7b7c84a04d29d77f
mcatm <tkcs@pelepop.com>
2020-05-22 13:55:01 +0900

    basic auth. #52

--- functions/src/ssr.ts
+++ functions/src/ssr.ts
@@ -1,22 +1,23 @@
-import * as express from 'express';
-const { Nuxt } = require('nuxt');
+import * as express from 'express'
+const basicAuth = require('basic-auth-connect')
+const { Nuxt } = require('nuxt')
 
 process.env.DEBUG = 'nuxt:*'
 
-const app = express();
+const app = express()
+
+app.all('/*', basicAuth(function(user: string, password: string) {
+  return user === process.env.BAUTH_USER && password === process.env.BAUTH_USER
+}))
+
 const nuxt = new Nuxt({
   dev: false,
-  // buildDir: '.nuxt',
   build: {
     publicPath: '/'
   }
-});
-
-// app.use(nuxt.render);
+})
 
 app.use(async (req: any, res: any) => {
-  // await nuxt.ready()
-  // return await nuxt.render(req, res)
   await nuxt.renderRoute(req.url, { req }).then((result: any) => {
     res.send(result.html)
   }).catch((e: any) => {
