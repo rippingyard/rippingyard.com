b3b35e419ba4ad178fa653f879310781c367c36f
mcatm <tkcs@pelepop.com>
2020-05-25 02:34:29 +0900

    fix #60

---
+++ components/atoms/Share/Tweet.vue
@@ -0,0 +1,31 @@
+<template>
+  <a :href="url" class="tweetButton" target="_blank"><i class="fab fa-twitter"></i> Tweet</a>
+</template>
+<script>
+export default {
+  props: {
+    href: {
+      default: '',
+      type: String,
+    },
+    title: {
+      default: 'rippingyard',
+      type: String,
+    }
+  },
+  data() {
+    return {
+      url: `http://twitter.com/intent/tweet?text=${ this.title }&url=${ this.href }&via=rippingyard`
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.tweetButton {
+  background: #00aced;
+  color: $white;
+  display: inline-block;
+  max-width: 200px;
+  padding: 10px 25px;
+}
+</style>
