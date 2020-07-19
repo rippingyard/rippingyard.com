e21cc8c2ff12e666a60cdc3cf846a81d0742218f
mcatm <tkcs@pelepop.com>
2020-07-19 12:26:00 +0900

    Init gtm only on production.

--- plugins/gtm.js
+++ plugins/gtm.js
@@ -1,24 +1,27 @@
 import Vue from 'vue'
 
 export default function({ $gtm, route }) {
-  console.log('init gtm')
-  $gtm.init('GTM-5B3N3TX')
 
-  Vue.mixin({
-    beforeRouteEnter: function(to, from, next) {
-      next(vm => {
-        setTimeout(() => {
-          console.log('pageView', document.title)
-          window.dataLayer.push(to.gtm || {
-            event: 'nuxtRoute',
-            pageType: 'PageView',
-            pageUrl: to.fullPath,
-            pageTitle: document.title,
-            // routeName: to.name,
-            routeName: document.title,
-          })
-        }, 1000)
-      })
-    }
-  })
+  if( process.env.NODE_ENV !== 'production' ) {
+    console.log('nothing init gtm on ', process.env.NODE_ENV)
+  } else {
+    $gtm.init('GTM-5B3N3TX')
+    Vue.mixin({
+      beforeRouteEnter: function(to, from, next) {
+        next(vm => {
+          setTimeout(() => {
+            console.log('pageView', document.title)
+            window.dataLayer.push(to.gtm || {
+              event: 'nuxtRoute',
+              pageType: 'PageView',
+              pageUrl: to.fullPath,
+              pageTitle: document.title,
+              // routeName: to.name,
+              routeName: document.title,
+            })
+          }, 1000)
+        })
+      }
+    })
+  }
 }
