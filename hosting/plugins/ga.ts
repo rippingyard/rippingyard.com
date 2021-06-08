546d9dac65f14783ee5082af662cb2b0874a1864
mcatm <tkcs@pelepop.com>
2021-06-08 15:15:52 +0900

    wip. #61

---
+++ hosting/plugins/ga.ts
@@ -0,0 +1,52 @@
+﻿import { GoogleApis } from 'googleapis'
+
+export function getPageview() {
+
+  const google = new GoogleApis()
+  const analytics = google.analyticsreporting('v4')
+  const credential: any = process.env.GA_CONFIG
+  const viewId = 'UA-104252-5'
+  const startDate = "2018-01-01"
+  const endDate = "2018-04-01"
+
+  console.log(process.env.GA_CONFIG)
+
+  const client = new google.auth.JWT(credential.client_email, '', credential.private_key, ["https://www.googleapis.com/auth/analytics.readonly"], '')
+
+  // client.authorize((error, _token) => {
+  //   if (error) return
+  //   analytics.reports.batchGet({
+  //     requestBody: {
+  //       reportRequests: [
+  //         {
+  //           "dateRanges": [
+  //             {
+  //               startDate,
+  //               endDate
+  //             }
+  //           ],
+  //           viewId,
+  //           "dimensions": [
+  //             {
+  //               "name": "ga:pagePath"
+  //             }
+  //           ],
+  //           "metrics": [
+  //             {
+  //               "expression": "ga:pageviews"
+  //             }
+  //           ],
+  //         }
+  //       ]
+  //     },
+  //     auth: client
+  //   }, (error: any, response: any) => {
+  //       if (error) console.log(error)
+  //       const pv = response.data.reports[0].data.rows[0].metrics
+  //       console.log('pv')
+  //       console.log(pv)
+  //   });
+  // })
+
+  return 610 // TOD: for test
+}
