3433fb5daeb4594906500ce9fc8a291e7f1c39c9
mcatm <tkcs@pelepop.com>
2022-02-16 01:29:32 +0900

    fix all

--- hosting/services/item.ts
+++ hosting/services/item.ts
@@ -3,6 +3,7 @@
   { label: '人物', key: 'person' },
   { label: '組織・グループ', key: 'group' },
   { label: '作品', key: 'work' },
+  { label: '場所', key: 'place' },
   { label: '出来事', key: 'event' },
   { label: 'キーワード', key: 'keyword' },
   { label: 'ブックマーク', key: 'bookmark' },
