6525ecb787543453f01ecb8093a638c700418e53
mcatm <tkcs@pelepop.com>
2023-03-29 01:36:13 +0900

    fix sitemap. #309

--- www/server/routes/sitemaps/posts.xml.ts
+++ www/server/routes/sitemaps/posts.xml.ts
@@ -1,4 +1,5 @@
-﻿import { Routes, buildSitemap } from '../../../utils/sitemap';
+﻿import dayjs from 'dayjs';
+import { Routes, buildSitemap } from '../../../utils/sitemap';
 
 export default defineEventHandler(async (event) => {
 
@@ -10,13 +11,20 @@ export default defineEventHandler(async (event) => {
       // lastmod: dayjs('2020-07-09 07:00'),
       // priority: '1.00',
     },
-    {
-      path: 'sitemaps/posts/2022',
+  ];
+
+  const thisYear = parseInt(dayjs().format('YYYY'));
+
+  console.log('thisYear', thisYear);
+
+  for (let i = 2012; i <= thisYear; i++) {
+    routes.push({
+      path: `sitemaps/posts/${i}.xml`,
       // lastmod: dayjs('2020-07-09 07:00'),
       // priority: '1.00',
       isIndex: true,
-    },
-  ];
+    })
+  }
 
   event.node.res.setHeader('content-type', 'text/xml');
   event.node.res.end(buildSitemap(routes));
