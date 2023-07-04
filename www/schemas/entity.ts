98e3214f2c58f610a2b656b74d26f40c50600c52
mcatm <tkcs@pelepop.com>
2023-07-05 00:53:55 +0900

    add entity type. #329

---
+++ www/schemas/entity.ts
@@ -0,0 +1,33 @@
+﻿import { Timestamp } from '@firebase/firestore-types'
+
+type EntityBody = {
+  name: string;
+};
+
+type EntityType = 'item' | 'place' | 'work' | 'keyword' | 'person' | 'group' | 'event' | 'bookmark' | 'unknown'
+
+export type OriginalEntity = {
+  id: string;
+  name: string;
+  type: EntityType;
+  description: string;
+  thumbnailImage: string;
+  images: string[];
+  entities: string[];
+  metadata?: { [key: string]: any };
+  createdAt: Timestamp;
+  updatedAt: Timestamp;
+  // hostGroup?: null;
+  isDeleted: boolean;
+  translated: {
+    [lang: string]: EntityBody;
+  };
+  counts: {
+    favorite: number;
+    bookmark: number;
+    pageview: number;
+  };
+  status: 'published' | 'draft';
+}
+
+export type Entity = OriginalEntity;
