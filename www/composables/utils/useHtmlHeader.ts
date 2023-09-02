9bf8a50070aac7c1c9bac337f47c71bca04d70d3
mcatm <tkcs@pelepop.com>
2023-09-02 23:17:18 +0900

    remove unnecessary lines

--- www/composables/utils/useHtmlHeader.ts
+++ www/composables/utils/useHtmlHeader.ts
@@ -15,14 +15,6 @@ export const useHtmlHeader = (meta: any = {}) => {
     ,
     ...meta,
   });
-  console.log('useHead', {
-    title,
-    titleTemplate: (chunk: string) => chunk && chunk !== title
-      ? `${chunk} - ${title}`
-      : `${title}`
-    ,
-    ...meta,
-  });
 
   if (gtm) {
     gtm.trackEvent({
