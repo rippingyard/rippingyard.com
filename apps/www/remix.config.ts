348833577dfb842cad3ec89d19bbd0ce88b9efb1
mcatm <tkcs@pelepop.com>
2024-01-12 23:24:52 +0900

    update

--- apps/www/remix.config.ts
+++ apps/www/remix.config.ts
@@ -1,9 +1,9 @@
 ﻿/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
+  // appDirectory: 'app',
+  // assetsBuildDirectory: 'public/build',
   ignoredRouteFiles: ['**/.*'],
-  publicPath: '/build/',
-  serverBuildPath: 'build/index.js',
+  // publicPath: '/build/',
+  // serverBuildPath: 'build/index.js',
   serverBuildTarget: 'cjs',
 };
