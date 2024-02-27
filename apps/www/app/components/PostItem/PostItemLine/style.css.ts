8a39243aeaad5f16362d51ebc7f81f1787d436b0
mcatm <tkcs@pelepop.com>
2024-02-27 22:47:46 +0900

    update

--- apps/www/app/components/PostItem/PostItemLine/style.css.ts
+++ apps/www/app/components/PostItem/PostItemLine/style.css.ts
@@ -1,15 +1,21 @@
-﻿import { style } from '@vanilla-extract/css';
+﻿import { globalStyle, style } from '@vanilla-extract/css';
 
 import { vars } from '~/styles/theme.css';
 import { rootVars } from '~/styles/vars.css';
 
-export const contentStyle = style({
-  display: 'block',
+export const containerStyle = style({
+  display: 'flex',
+  gap: 16,
+  alignItems: 'center',
   padding: '16px 0',
   borderBottom: `1px solid ${vars.color.shadow}`,
   ':hover': {
     backgroundColor: vars.color.highlight,
   },
+});
+
+export const contentStyle = style({
+  width: '100%',
 });
 
 export const headingStyle = style({
@@ -24,4 +30,18 @@ export const summaryStyle = style({
 
 export const footerStyle = style({
   fontSize: rootVars.font.size.tiny,
+});
+
+export const imageStyle = style({
+  flexShrink: 0,
+  width: 90,
+  height: 90,
+  borderRadius: 999,
+  overflow: 'hidden',
+});
+
+globalStyle(`${imageStyle} > img`, {
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
 });
