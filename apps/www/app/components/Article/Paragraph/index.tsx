1838dc2380482b069e49793a9bc54402b85a18f8
mcatm <tkcs@pelepop.com>
2024-03-10 22:40:33 +0900

    hotfix

--- apps/www/app/components/Article/Paragraph/index.tsx
+++ apps/www/app/components/Article/Paragraph/index.tsx
@@ -2,7 +2,7 @@
 
 import { ContentBlock } from '~/hooks/normalize/useContentBlocks';
 import { useContentHTML } from '~/hooks/normalize/useContentHTML';
-import { extractUrls } from '~/utils/typography';
+import { extractUrls, stripTags } from '~/utils/typography';
 
 import { Link } from '../Link';
 
@@ -11,9 +11,9 @@ export const Paragraph: FC<{ block: ContentBlock }> = ({ block }) => {
   const content = useContentHTML(block);
 
   const urls = extractUrls(content);
-  if (urls.length > 0) console.log('urls', urls);
 
-  if (urls.length === 1 && content === urls[0]) return <Link url={urls[0]} />;
+  if (urls.length === 1 && stripTags(content) === urls[0])
+    return <Link url={urls[0]} />;
 
   return <p dangerouslySetInnerHTML={{ __html: content }} />;
 };
