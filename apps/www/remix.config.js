bc921f7a99e743b5b9cec15ebf00aa538b2eaa33
mcatm <tkcs@pelepop.com>
2024-03-14 14:29:12 +0900

    remix config

--- apps/www/remix.config.js
+++ apps/www/remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ['**/.*'],
+  serverMinify: true,
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
