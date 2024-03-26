27f863727bfe7df45a0de88f7c197466842af973
mcatm <tkcs@pelepop.com>
2024-03-26 14:20:34 +0900

    fix #452

--- apps/www/remix.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ignoredRouteFiles: ['**/.*'],
-  serverMinify: true,
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
-};
---
+++ apps/www/vite.config.ts
@@ -0,0 +1,24 @@
+﻿import { vitePlugin as remix } from '@remix-run/dev';
+import { installGlobals } from '@remix-run/node';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+// import { remixDevTools } from 'remix-development-tools/vite';
+import { vercelPreset } from '@vercel/remix/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+installGlobals();
+
+export default defineConfig({
+  server: {
+    port: 3334,
+  },
+  plugins: [
+    // remixDevTools(),
+    vanillaExtractPlugin(),
+    remix({
+      presets: [vercelPreset()],
+      ignoredRouteFiles: ['**/.*'],
+    }),
+    tsconfigPaths(),
+  ],
+});
