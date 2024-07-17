20cb9a303902890493e83ea20290904099ad4f6c
mcatm <tkcs@pelepop.com>
2024-07-18 01:39:01 +0900

    console.log

--- apps/www/app/hooks/firebase/useFirebase.server.ts
+++ apps/www/app/hooks/firebase/useFirebase.server.ts
@@ -6,20 +6,18 @@ export const useFirebase = () => {
     process.env.VITE_GOOGLE_APPLICATION_CREDENTIALS || ''
   );
   console.log('cert', cert);
-  console.log(
-    'process.env.VITE_FIREBASE_STORAGE_BUCKET',
-    process.env.VITE_FIREBASE_STORAGE_BUCKET
-  );
-  console.log(
-    'process.env.VITE_FIREBASE_DATABASE_URL',
-    process.env.VITE_FIREBASE_DATABASE_URL
-  );
   if (!admin.apps.length) {
+    console.log(
+      'initialize! VITE_FIREBASE_STORAGE_BUCKET',
+      process.env.VITE_FIREBASE_STORAGE_BUCKET
+    );
     initializeApp({
       credential: admin.credential.cert(cert),
       databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
       storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
     });
+  } else {
+    console.log('no initialize');
   }
   return admin;
 };
