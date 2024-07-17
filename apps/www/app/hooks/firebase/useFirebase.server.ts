6a84e5e5739d3aa86aa5dba11c4784447095afa2
mcatm <tkcs@pelepop.com>
2024-07-18 01:48:29 +0900

    FIREBASE_CONFIG

--- apps/www/app/hooks/firebase/useFirebase.server.ts
+++ apps/www/app/hooks/firebase/useFirebase.server.ts
@@ -6,18 +6,13 @@ export const useFirebase = () => {
     process.env.VITE_GOOGLE_APPLICATION_CREDENTIALS || ''
   );
   console.log('cert', cert);
-  // if (!admin.apps.length) {
-  console.log(
-    'initialize! VITE_FIREBASE_STORAGE_BUCKET',
-    process.env.VITE_FIREBASE_STORAGE_BUCKET
-  );
-  initializeApp({
-    credential: admin.credential.cert(cert),
-    databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
-    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-  });
-  // } else {
-  //   console.log('no initialize');
-  // }
+  console.log('process.env.FIREBASE_CONFIG', process.env.FIREBASE_CONFIG);
+  if (!admin.apps.length) {
+    initializeApp({
+      credential: admin.credential.cert(cert),
+      databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
+      storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    });
+  }
   return admin;
 };
