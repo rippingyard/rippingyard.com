3ea4d6c11f1e7b8a50f533823eccc917ce5ba869
mcatm <tkcs@pelepop.com>
2024-08-06 10:37:15 +0900

    fix #600

--- apps/www/app/hooks/cache/useCache.ts
+++ apps/www/app/hooks/cache/useCache.ts
@@ -4,18 +4,48 @@ export enum CACHE_KEYS {
   PUBLIC_POSTS = 'publicPosts',
 }
 
+type CacheObject<T> = {
+  expired: number;
+  items: SerializeFrom<T>[];
+};
+
+const cacheItems = <T>(
+  key: CACHE_KEYS,
+  items: T[],
+  expired: number = 600000
+): void => {
+  if (typeof sessionStorage === 'undefined') return;
+  console.log('expiredAt', Date.now() + expired);
+  sessionStorage.setItem(
+    key,
+    JSON.stringify({
+      expired: Date.now() + expired,
+      items,
+    })
+  );
+};
+
 const getCachedItems = <T>(key: string): SerializeFrom<T>[] => {
   if (typeof sessionStorage === 'undefined') return [];
   const cache = sessionStorage.getItem(key);
-  return cache ? JSON.parse(cache) : [];
+  if (!cache) return [];
+
+  const cacheObject: CacheObject<T> = JSON.parse(cache);
+  const now = Date.now();
+
+  if (cacheObject?.expired < now) {
+    sessionStorage.removeItem(key);
+    return [];
+  }
+  console.log('cacheObject?.items', cacheObject?.items);
+  return cacheObject?.items || [];
 };
 
 const clearCachedItems = () => {
   if (typeof sessionStorage === 'undefined') return;
   Object.values(CACHE_KEYS).forEach((key) => {
-    console.log('CACHE_KEYS', key);
     sessionStorage.removeItem(key);
   });
 };
 
-export { getCachedItems, clearCachedItems };
+export { cacheItems, getCachedItems, clearCachedItems };
