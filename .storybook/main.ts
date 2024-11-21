4292c4a4782ef696dfe01f38c9fef28e93823297
mcatm <tkcs@pelepop.com>
2024-11-22 00:47:49 +0900

    fix chromium

---
+++ .storybook/main.ts
@@ -0,0 +1,43 @@
+﻿import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+
+const config: StorybookConfig = {
+  stories: [
+    // "../**/*.mdx",
+    "../apps/www/components/**/*.stories.@(ts|tsx)",
+  ],
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  addons: [
+    // '@storybook/addon-onboarding',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    // '@chromatic-com/storybook',
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  previewHead: (head) => `
+    ${head}
+    <link rel="stylesheet" href="https://unpkg.com/destyle.css@4.0.1/destyle.min.css">
+    <style>
+      html, body {
+        font-size: 18px;
+        background: hsla(0, 0%, 96%, 1);
+      }
+    </style>
+ `,
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      plugins: [vanillaExtractPlugin()],
+    });
+  },
+};
+export default config;
---
+++ .storybook/preview.tsx
@@ -0,0 +1,26 @@
+﻿import type { Preview } from "@storybook/react";
+import React from "react";
+import { FC } from "react";
+// import { bodyStyle } from "../app/styles/root.css";
+// import { themeClass } from "../app/styles/theme.css";
+// import "../app/styles/vars.css";
+// import clsx from "clsx";
+
+const Styled = (Story: FC) => {
+  return (
+    // <div className={clsx(bodyStyle, themeClass)}>
+    <div>
+      <Story />
+    </div>
+  );
+};
+
+export const decorators = [Styled];
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+  },
+};
+
+export default preview;
