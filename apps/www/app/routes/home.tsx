ebee0414ad98a01cc2d7b149e775e920a03d3594
mcatm <tkcs@pelepop.com>
2024-11-24 22:36:32 +0900

    add cache. #941

--- apps/www/app/routes/home.tsx
+++ apps/www/app/routes/home.tsx
@@ -1,8 +1,16 @@
 ﻿import { Outlet } from '@remix-run/react';
 import { json, redirect, type LoaderFunctionArgs } from '@vercel/remix';
+import dayjs from 'dayjs';
+import { Timestamp } from 'firebase-admin/firestore';
 
 import { Heading } from '~/components/Heading';
+import { QueryParams } from '~/hooks/condition/usePostConditions';
+import { useCache } from '~/hooks/fetch/useCache.server';
+import { usePosts } from '~/hooks/fetch/usePosts.server';
+import { useDocReference } from '~/hooks/firestore/useDocReference.server';
+import { useSaveCache } from '~/hooks/save/useSaveCache.server';
 import { commitSession, getMe, getSession } from '~/middlewares/session.server';
+import { Post } from '~/schemas/post';
 import { wideContainerStyle } from '~/styles/container.css';
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -16,7 +24,50 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       },
     });
   }
-  return json({});
+
+  const cacheKey = `my-tags-${uid}`;
+  const cache = await useCache<string[]>(cacheKey);
+  const saveCache = useSaveCache();
+
+  // TODO: この辺の処理は、フック化できるはず
+  let myTags: string[] = [];
+  if (cache) {
+    console.log('cached!', cacheKey);
+    myTags = cache;
+  } else {
+    const args: Omit<QueryParams<Post>, 'collection'> = {
+      limit: 100,
+      myId: uid,
+      removeWhereKeys: ['isPublic'],
+      where: [
+        {
+          key: 'owner',
+          val: useDocReference(uid, 'users'),
+        },
+      ],
+      orderBy: {
+        key: 'publishedAt',
+        order: 'desc',
+      },
+    };
+    const { data: items } = await usePosts(args);
+
+    for (const item of items) {
+      if (!item?.tags) continue;
+      const tags = item.tags.filter((tag) => tag && !myTags.includes(tag));
+      myTags.push(...tags);
+    }
+
+    const expiredAt = Timestamp.fromDate(dayjs().add(1, 'day').toDate());
+
+    if (myTags.length > 0)
+      await saveCache({
+        id: cacheKey,
+        body: myTags,
+        expiredAt,
+      });
+  }
+  return json({ myTags });
 };
 
 export default function Index() {
