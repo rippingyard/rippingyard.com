005de442092a9a78ee8881334d9b04496e908e1e
mcatm <hamada@cinra.net>
2025-02-01 02:46:12 +0900

    import template for vercel. #1012

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -1,31 +1,23 @@
 import { reactRouter } from '@react-router/dev/vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-// import { vercelPreset } from '@vercel/remix/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // const isStorybook = process.argv[1]?.includes('storybook');
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild, command }) => ({
   server: {
     port: 3334,
   },
-  plugins: [
-    // remixDevTools(),
-    vanillaExtractPlugin(),
-    reactRouter(),
-    // !isStorybook &&
-    //   reactRouter({
-    //     presets: [vercelPreset()],
-    //     ignoredRouteFiles: ['**/.*'],
-    //     future: {
-    //       unstable_optimizeDeps: true,
-    //       v3_routeConfig: true,
-    //     },
-    //   }),
-    tsconfigPaths(),
-  ],
-  // optimizeDeps: {
-  //   include: ['@remix-run/react'], ///  <-- add @remix-run/react to optimized deps
-  // },
-});
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: './server/app.ts',
+        }
+      : undefined,
+  },
+  ssr: {
+    noExternal: command === 'build' ? true : undefined,
+  },
+  plugins: [vanillaExtractPlugin(), reactRouter(), tsconfigPaths()],
+}));
