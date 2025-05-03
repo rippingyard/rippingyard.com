6c17d3f9c01c8fecefe57382f90f88ee934de1ac
mcatm <tkcs@pelepop.com>
2025-05-03 15:42:40 +0900

    work on paste image. #1100

---
+++ apps/www/app/hooks/ui/useImageEditor.ts
@@ -0,0 +1,69 @@
+﻿import { Image } from '@tiptap/extension-image';
+import { Plugin } from 'prosemirror-state';
+
+import { resizeImage } from '~/utils/image';
+
+import { useUploadImage } from '../save/useUploadImage';
+
+export const useImageEditor = ({ uploadpath }: { uploadpath: string }) => {
+  const { uploadImage } = useUploadImage({ uploadpath });
+
+  return Image.extend({
+    addProseMirrorPlugins() {
+      return [
+        new Plugin({
+          props: {
+            handleDOMEvents: {
+              paste(view, event) {
+                console.log('event', event);
+                console.log('view', view);
+
+                try {
+                  const hasFiles =
+                    event.clipboardData &&
+                    event.clipboardData.files &&
+                    event.clipboardData.files.length;
+
+                  if (!hasFiles) return;
+
+                  const images = Array.from(event.clipboardData.files).filter(
+                    (file) => /image/i.test(file.type)
+                  );
+
+                  if (images.length === 0) return;
+
+                  event.preventDefault();
+
+                  const { schema } = view.state;
+
+                  images.forEach((image) => {
+                    console.log('image', image);
+                    const reader = new FileReader();
+
+                    reader.onload = async () => {
+                      const resizedImage = await resizeImage(image);
+                      const uploadedImage = await uploadImage({
+                        file: resizedImage,
+                      });
+
+                      const node = schema.nodes.image.create({
+                        src: uploadedImage.url,
+                      });
+                      const transaction =
+                        view.state.tr.replaceSelectionWith(node);
+                      view.dispatch(transaction);
+                    };
+
+                    reader.readAsDataURL(image);
+                  });
+                } catch (e) {
+                  console.error(e);
+                }
+              },
+            },
+          },
+        }),
+      ];
+    },
+  });
+};
