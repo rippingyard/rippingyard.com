b0b749cdc99ac58b6c40109c51f272faf5bf5227
mcatm <tkcs@pelepop.com>
2025-05-03 16:27:34 +0900

    implemantation about drop. #1100

--- apps/www/app/hooks/ui/useImageEditor.ts
+++ apps/www/app/hooks/ui/useImageEditor.ts
@@ -5,6 +5,9 @@ import { resizeImage } from '~/utils/image';
 
 import { useUploadImage } from '../save/useUploadImage';
 
+const getImages = (files: FileList) =>
+  !files ? [] : Array.from(files).filter((file) => /image/i.test(file.type));
+
 export const useImageEditor = ({ uploadpath }: { uploadpath: string }) => {
   const { uploadImage } = useUploadImage({ uploadpath });
 
@@ -14,6 +17,54 @@ export const useImageEditor = ({ uploadpath }: { uploadpath: string }) => {
         new Plugin({
           props: {
             handleDOMEvents: {
+              drop(view, event) {
+                console.log('event', event);
+                console.log('view', view);
+
+                try {
+                  const hasFiles =
+                    event.dataTransfer &&
+                    event.dataTransfer.files &&
+                    event.dataTransfer.files.length;
+
+                  if (!hasFiles) return;
+
+                  const images = getImages(event.dataTransfer.files);
+
+                  if (images.length === 0) return;
+
+                  event.preventDefault();
+
+                  const { schema } = view.state;
+                  const coordinates = view.posAtCoords({
+                    left: event.clientX,
+                    top: event.clientY,
+                  });
+
+                  images.forEach((image) => {
+                    const reader = new FileReader();
+                    reader.onload = async () => {
+                      const resizedImage = await resizeImage(image);
+                      const uploadedImage = await uploadImage({
+                        file: resizedImage,
+                      });
+
+                      const node = schema.nodes.image.create({
+                        src: uploadedImage.url,
+                      });
+                      const transaction = view.state.tr.insert(
+                        coordinates?.pos as number,
+                        node
+                      );
+                      view.dispatch(transaction);
+                    };
+
+                    reader.readAsDataURL(image);
+                  });
+                } catch (e) {
+                  console.error(e);
+                }
+              },
               paste(view, event) {
                 console.log('event', event);
                 console.log('view', view);
@@ -26,9 +77,7 @@ export const useImageEditor = ({ uploadpath }: { uploadpath: string }) => {
 
                   if (!hasFiles) return;
 
-                  const images = Array.from(event.clipboardData.files).filter(
-                    (file) => /image/i.test(file.type)
-                  );
+                  const images = getImages(event.clipboardData.files);
 
                   if (images.length === 0) return;
 
@@ -37,7 +86,6 @@ export const useImageEditor = ({ uploadpath }: { uploadpath: string }) => {
                   const { schema } = view.state;
 
                   images.forEach((image) => {
-                    console.log('image', image);
                     const reader = new FileReader();
 
                     reader.onload = async () => {
