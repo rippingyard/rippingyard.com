5eb78521b34d9d3182176debdf93762ad6857d6e
mcatm <tkcs@pelepop.com>
2025-05-09 00:10:24 +0900

    remove comments. #1100

--- apps/www/app/hooks/ui/useImageEditor.ts
+++ apps/www/app/hooks/ui/useImageEditor.ts
@@ -17,58 +17,7 @@ export const useImageEditor = ({ uploadpath }: { uploadpath: string }) => {
         new Plugin({
           props: {
             handleDOMEvents: {
-              // drop(view, event) {
-              //   console.log('event', event);
-              //   console.log('view', view);
-
-              //   try {
-              //     const hasFiles =
-              //       event.dataTransfer &&
-              //       event.dataTransfer.files &&
-              //       event.dataTransfer.files.length;
-
-              //     if (!hasFiles) return;
-
-              //     const images = getImages(event.dataTransfer.files);
-
-              //     if (images.length === 0) return;
-
-              //     event.preventDefault();
-
-              //     const { schema } = view.state;
-              //     const coordinates = view.posAtCoords({
-              //       left: event.clientX,
-              //       top: event.clientY,
-              //     });
-
-              //     images.forEach((image) => {
-              //       const reader = new FileReader();
-              //       reader.onload = async () => {
-              //         const resizedImage = await resizeImage(image);
-              //         const uploadedImage = await uploadImage({
-              //           file: resizedImage,
-              //         });
-
-              //         const node = schema.nodes.image.create({
-              //           src: uploadedImage.url,
-              //         });
-              //         const transaction = view.state.tr.insert(
-              //           coordinates?.pos as number,
-              //           node
-              //         );
-              //         view.dispatch(transaction);
-              //       };
-
-              //       reader.readAsDataURL(image);
-              //     });
-              //   } catch (e) {
-              //     console.error(e);
-              //   }
-              // },
               paste(view, event) {
-                console.log('event', event);
-                console.log('view', view);
-
                 try {
                   const hasFiles =
                     event.clipboardData &&
