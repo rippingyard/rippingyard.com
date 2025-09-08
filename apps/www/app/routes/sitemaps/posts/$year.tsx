d9e2e7a76c5ab18e2db6d46eb4ce94f4ae704d89
mcatm <hamada@cinra.net>
2025-09-08 13:33:31 +0900

    update sitemap errors.

--- apps/www/app/routes/sitemaps/posts/$year.tsx
+++ apps/www/app/routes/sitemaps/posts/$year.tsx
@@ -13,50 +13,55 @@ import {
 import { useFirestore } from '~/hooks/firestore/useFirestore';
 import { Routes, buildSitemap } from '~/utils/sitemap';
 
-import { Route } from '../+types/seeds';
+import { Route } from './+types/$year';
 
 export const loader = async ({ params }: Route.LoaderArgs) => {
-  const { year } = params;
-  if (!year) {
-    throw new Error();
-  }
-  console.log('year', `${year + 1}-01-01 00:00:00`);
+  try {
+    const { year } = params;
+    if (!year) {
+      throw new Error();
+    }
+    console.log('year', `${Number(year) + 1}-01-01 00:00:00`);
 
-  const { db } = useFirestore();
+    const { db } = useFirestore();
 
-  const routes: Routes = [];
+    const routes: Routes = [];
 
-  const q = query(
-    collection(db, 'posts'),
-    where('isPublic', '==', true),
-    where('isDeleted', '==', false),
-    where('status', '==', 'published'),
-    orderBy('publishedAt', 'desc'),
-    endAt(Timestamp.fromDate(new Date(`${parseInt(year)}-01-01 00:00:00`))),
-    startAfter(
-      Timestamp.fromDate(new Date(`${parseInt(year) + 1}-01-01 00:00:00`))
-    )
-  );
+    const q = query(
+      collection(db, 'posts'),
+      where('isPublic', '==', true),
+      where('isDeleted', '==', false),
+      where('status', '==', 'published'),
+      orderBy('publishedAt', 'desc'),
+      endAt(Timestamp.fromDate(new Date(`${parseInt(year)}-01-01 00:00:00`))),
+      startAfter(
+        Timestamp.fromDate(new Date(`${parseInt(year) + 1}-01-01 00:00:00`))
+      )
+    );
 
-  const snapshot = await getDocs(q);
+    const snapshot = await getDocs(q);
 
-  snapshot.forEach((doc) => {
-    const post = doc.data();
-    routes.push({
-      path: `post/${doc.id}`,
-      lastmod: dayjs(post.updatedAt.toDate()),
-      // priority: '1.00',
+    snapshot.forEach((doc) => {
+      const post = doc.data();
+      routes.push({
+        path: `post/${doc.id}`,
+        lastmod: dayjs(post.updatedAt.toDate()),
+        // priority: '1.00',
+      });
     });
-  });
 
-  // Return the response with the content, a status 200 message, and the appropriate headers for an XML page
-  return new Response(buildSitemap(routes), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/xml',
-      'Cache-Control': 'public, max-age=3600',
-      'xml-version': '1.0',
-      encoding: 'UTF-8',
-    },
-  });
+    // Return the response with the content, a status 200 message, and the appropriate headers for an XML page
+    return new Response(buildSitemap(routes), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/xml',
+        'Cache-Control': 'public, max-age=3600',
+        'xml-version': '1.0',
+        encoding: 'UTF-8',
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
