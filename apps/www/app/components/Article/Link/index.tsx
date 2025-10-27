b3c3a243ae1ad1eb98e50b389c01cf3f5ec1e691
mcatm <hamada@cinra.net>
2025-10-28 01:09:52 +0900

    quickfix

--- apps/www/app/components/Article/Link/index.tsx
+++ apps/www/app/components/Article/Link/index.tsx
@@ -1,18 +1,19 @@
 ﻿import { FC } from 'react';
 
 import { YouTube } from '~/components/YouTube';
-import { isInstagramUrl, isTwitterUrl, isYouTubeUrl } from '~/utils/typography';
+import { isYouTubeUrl } from '~/utils/typography';
+// import { isInstagramUrl, isTwitterUrl, isYouTubeUrl } from '~/utils/typography';
 
 import { EmbedLink } from './EmbedLink';
-import { Instagram } from './Instagram';
-import { Twitter } from './Twitter';
+// import { Instagram } from './Instagram';
+// import { Twitter } from './Twitter';
 
 export const Link: FC<{ url: string }> = ({ url }) => {
   if (!url) return;
 
   if (isYouTubeUrl(url)) return <YouTube url={url} />;
-  if (isInstagramUrl(url)) return <Instagram url={url} />;
-  if (isTwitterUrl(url)) return <Twitter url={url} />;
+  // if (isInstagramUrl(url)) return <Instagram url={url} />;
+  // if (isTwitterUrl(url)) return <Twitter url={url} />;
 
   return <EmbedLink url={url} />;
 };
