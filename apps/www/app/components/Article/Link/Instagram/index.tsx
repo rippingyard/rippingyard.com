2106cd63146f1fa65bb798d5e062c092602b3c2c
mcatm <hamada@cinra.net>
2025-10-28 10:05:46 +0900

    testing

--- apps/www/app/components/Article/Link/Instagram/index.tsx
+++ apps/www/app/components/Article/Link/Instagram/index.tsx
@@ -4,11 +4,12 @@ import { InstagramEmbed } from 'react-social-media-embed';
 import * as styles from './styles.css';
 import { NormalLink } from '../NormalLink';
 
-export const Instagram: FC<{ url: string; showExternalEmbed: boolean }> = ({
-  url,
-  showExternalEmbed = false,
-}) => {
-  if (!showExternalEmbed) return <NormalLink url={url} />;
+export const Instagram: FC<{
+  url: string;
+  showExternalEmbed: boolean;
+  isTesting: boolean;
+}> = ({ url, showExternalEmbed = false, isTesting = true }) => {
+  if (!showExternalEmbed || isTesting) return <NormalLink url={url} />;
   return (
     <div className={styles.container}>
       <InstagramEmbed url={url} width="100%" />
--- apps/www/app/components/Article/Link/Twitter/index.tsx
+++ apps/www/app/components/Article/Link/Twitter/index.tsx
@@ -4,11 +4,15 @@ import { XEmbed } from 'react-social-media-embed';
 import * as styles from './style.css';
 import { NormalLink } from '../NormalLink';
 
-export const Twitter: FC<{ url: string; showExternalEmbed: boolean }> = ({
-  url,
-  showExternalEmbed = false,
-}) => {
-  if (!showExternalEmbed) return <NormalLink url={url} />;
+export const Twitter: FC<{
+  url: string;
+  showExternalEmbed: boolean;
+  isTesting: boolean;
+}> = ({ url, showExternalEmbed = false, isTesting = true }) => {
+  const tweetId = url.substring(url.lastIndexOf('/') + 1).replace(/[?].*$/, '');
+  console.log('tweetId', tweetId);
+
+  if (!showExternalEmbed || isTesting) return <NormalLink url={url} />;
   return (
     <div className={styles.container}>
       <XEmbed url={url} width="100%" />
